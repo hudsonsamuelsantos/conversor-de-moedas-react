3a3131f76cd9960e7edd266c22bf641758c0ef98
Antonio Lucio <antonioluciofb@hotmail.com>
2022-06-02 09:51:03 -0300

    improvements

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
-import Conversor from './components/Conversor';
+import "./App.css";
+import Conversor from "./components/Conversor";
 
 function App() {
   return (
     <div className="App">
       <h1>Conversor de Moeda$</h1>
       <div className="linha">
-        <Conversor moedaA="BRL" moedaB="USD" />
-        <Conversor moedaA="USD" moedaB="BRL" />
+        <Conversor firstCurrency="BRL" secondCurrency="USD" />
+        <Conversor firstCurrency="USD" secondCurrency="BRL" />
       </div>
-      <div className='linha'>
-        <Conversor moedaA="BRL" moedaB="EUR" />
-        <Conversor moedaA="EUR" moedaB="BRL" />
+      <div className="linha">
+        <Conversor firstCurrency="BRL" secondCurrency="EUR" />
+        <Conversor firstCurrency="EUR" secondCurrency="BRL" />
       </div>
-      <div className='linha'>
-        <Conversor moedaA="BRL" moedaB="CAD" />
-        <Conversor moedaA="CAD" moedaB="BRL" />
+      <div className="linha">
+        <Conversor firstCurrency="BRL" secondCurrency="CAD" />
+        <Conversor firstCurrency="CAD" secondCurrency="BRL" />
       </div>
       <footer>Hudson Santos - Front-end React Developer</footer>
     </div>
