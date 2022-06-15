cd353629c496dc9e3d336f5e939dd635af9fcac1
Hudson Santos <hudsnss@gmail.com>
2022-06-14 21:56:54 -0300

    improvements

--- src/App.js
+++ src/App.js
@@ -4,19 +4,7 @@ import Conversor from "./components/Conversor";
 function App() {
   return (
     <div className="App">
-      <h1>Conversor de Moeda$</h1>
-      <div className="linha">
-        <Conversor firstCurrency="BRL" secondCurrency="USD" />
-        <Conversor firstCurrency="USD" secondCurrency="BRL" />
-      </div>
-      <div className="linha">
-        <Conversor firstCurrency="BRL" secondCurrency="EUR" />
-        <Conversor firstCurrency="EUR" secondCurrency="BRL" />
-      </div>
-      <div className="linha">
-        <Conversor firstCurrency="BRL" secondCurrency="CAD" />
-        <Conversor firstCurrency="CAD" secondCurrency="BRL" />
-      </div>
+      <Conversor />
       <footer>Hudson Santos - Front-end React Developer</footer>
     </div>
   );
--- src/components/Conversor.jsx
+++ src/components/Conversor.jsx
@@ -3,9 +3,12 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 import "./Conversor.css";
 
-export default function Conversor({ firstCurrency, secondCurrency }) {
+export default function Conversor() {
   const [value, setValue] = useState("");
   const [result, setResult] = useState();
+
+  const [firstCurrency, setFirstCurrency] = useState("")
+  const [secondCurrency, setSecondCurrency] = useState("")
 
   const handleConvertMoney = async () => {
     const url = `https://api.apilayer.com/currency_data/convert?to=${secondCurrency}&from=${firstCurrency}&amount=${value}`;
@@ -25,12 +28,33 @@ export default function Conversor({ firstCurrency, secondCurrency }) {
 
   return (
     <div className="conversor">
-      <h1>
-        {firstCurrency} para {secondCurrency}
-      </h1>
-      <input type="text" onChange={(e) => setValue(e.target.value)} />
+      <h1>Conversor de Moeda$</h1>
+
+      <div className="inputAndCurrency">
+        <input type="text" onChange={(e) => setValue(e.target.value)} />
+
+        <select name="select" onChange={(e) => setFirstCurrency(e.target.value)}>
+          <option value="">De</option>
+          <option value="BRL">Real</option>
+          <option value="USD">Dólar</option>
+          <option value="EUR">Euro</option>
+          <option value="CAD">Dólar Canadense</option>
+        </select>
+      </div>
+
+      <div className="inputAndCurrency">
+        <input type="text" readOnly value={result} />
+
+        <select name="select" onChange={(e) => setSecondCurrency(e.target.value)}>
+          <option value="">Para</option>
+          <option value="USD">Dólar</option>
+          <option value="BRL">Real</option>
+          <option value="EUR">Euro</option>
+          <option value="CAD">Dólar Canadense</option>
+        </select>
+      </div>
+
       <button onClick={handleConvertMoney}>Converter</button>
-      <p>Valor convertido: {result}</p>
     </div>
   );
 }
